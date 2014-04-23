1214b5a97b294b66cf4acaf76f2d537c52e706cb
Prelang Builder <builder@prelang.com>
2014-04-23 23:08:30 +0000

    Generated scaffolding for model 'Schedule' via Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Prelang::Application.routes.draw do
+  resources :schedules
+
   devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
